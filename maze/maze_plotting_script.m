78f26307cb30a534421c760c40760e2b5f6d5798
HitchedLime <71065096+HitchedLime@users.noreply.github.com>
2024-08-12 13:41:29 +0200

    Add files via upload

--- maze/maze_plotting_script.m
+++ maze/maze_plotting_script.m
@@ -10,32 +10,22 @@ clear all;
 
 %%
 
-maze_1;
-start = [0, 0];
-end_ = [5,0];
+maze_1_3D;
+
+start  =[1 1 2]
+end_ = [2,1,2]
+
+%start = [0, 0];
+%end_ = [5,0];
 
 fig_num = 1;
+
 
 plot_map(map, fig_num);
 plot_start_stop(start, end_, fig_num)
 
-route = [1 1; 2 1; 3 1; 4 1; 5 1; 6 1];
+
+
+route = greedy_3d(map,start,end_)
 
 plot_route(route, fig_num);
-
-%%
-
-fig_num = 2;
-
-maze_1_3D;
-
-start = [0 0 0];
-end_ = [5 0 2];
-
-plot_map(map, fig_num, 0);
-plot_start_stop(start, end_, fig_num)
-
-route = [1 1 1; 2 1 2; 3 1 3; 4 1 3; 5 1 3; 6 1 3];
-
-plot_route(route, fig_num)
-
